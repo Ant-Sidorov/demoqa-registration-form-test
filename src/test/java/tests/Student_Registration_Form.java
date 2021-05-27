adee46a8f97aa3151d902326cd853304bf40e443
xclu7ive <xclu7ive@gmail.com>
2021-05-27 03:28:13 +0300

    test

--- src/test/java/tests/Student_Registration_Form.java
+++ src/test/java/tests/Student_Registration_Form.java
@@ -18,28 +18,28 @@ public class Student_Registration_Form {
     void successfulSubmitForm() {
         open("https://demoqa.com/automation-practice-form");
 
-        $("[id=firstName]").setValue("Anton");
-        $("[id=lastName]").setValue("Sidorov");
-        $("[id=userEmail]").setValue("ant@mail.com");
-        $("[for='gender-radio-1']").click();
-        $("[id=userNumber]").setValue("1234567890");
-        $("[id=dateOfBirthInput]").click();
-        $("[class=react-datepicker__month-select]").selectOption(3);
-        $("[class=react-datepicker__year-select]").selectOption("1991");
+        $("#firstName").setValue("Anton");
+        $("#lastName").setValue("Sidorov");
+        $("#userEmail").setValue("ant@mail.com");
+        $("[for=gender-radio-1]").click();
+        $("#userNumber").setValue("1234567890");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption(3);
+        $(".react-datepicker__year-select").selectOption("1991");
         $("[aria-label=\"Choose Monday, April 8th, 1991\"]").click();
 
 
-        $("[id=subjectsInput]").setValue("Commerce").pressEnter();
+        $("#subjectsInput").setValue("Commerce").pressEnter();
 
         $("[for=hobbies-checkbox-1]").click();
-        File file = $("[id=uploadPicture]").uploadFile(new File("src/test/resources/test_upload.jpg"));
+        File file = $("#uploadPicture").uploadFile(new File("src/test/resources/test_upload.jpg"));
 
-        $("[id=currentAddress]").setValue("Sport street");
+        $("#currentAddress").setValue("Sport street");
 
-        $("[id=react-select-3-input]").setValue("NCR").pressEnter();
-        $("[id=react-select-4-input]").setValue("Gurgaon").pressEnter();
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Gurgaon").pressEnter();
 
-        $("[id=submit]").click();
+        $("#submit").click();
 
 
         sleep(5000);
